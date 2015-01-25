d00731231a1178e377897ac7099240413c84cbe0
koskot77 <koskot77@hotmail.com>
2015-01-25 21:04:57 +0100

    some info

--- ui.R
+++ ui.R
@@ -19,8 +19,8 @@ shinyUI(pageWithSidebar(
 #    dateInput("date", "Date:")
   ),
   mainPanel(
+    p('The Twitter platform has gained millions of users from all around the world and it is generating an endless stream of data. Although, Twitter is also used routinely by PR departments of corporations, universities, and heads of states to propagate their agendas to public, the majority of tweets still comes from regular users and can possibly be used to as a probe of a "public mood". The idea has drawn a lot of attention from the academic community and there was one particularly interesting publication on', a("crowd behavior", href="http://arxiv.org/abs/1402.2308", target="_blank"),'.'),
+    p('Using twitter API we have collected a decent number of tweets coming from a particular place in the world. A few very basic observables were selected and added to this application. You can choose between 3 of those observables and visualize 1-dimensional distributions and 2-dimensionals scatter plots. In addition you can zoom in and out these plots using the slider bars.'),
     plotOutput('myHist')
-#,
-#    p('some ordinary text')
   )
 ))
