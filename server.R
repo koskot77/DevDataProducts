f95b60acd58c49be1b69f0f24127ea195ad0b231
koskot77 <koskot77@hotmail.com>
2015-01-26 00:47:33 +0100

    events captured

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ ukr$dt  <- as.POSIXct( ukr$DateTime, format="%Y-%m-%d %H:%M:%S",  tz = "GMT")
 
 shinyServer(
   function(input, output) {
+    nEvents <- 0
     output$myHist <- renderPlot({
       x <- input$inputX
       y <- input$inputY
@@ -47,7 +48,30 @@ shinyServer(
           theme(title = element_text(size = 15), axis.title.x = element_text(size = 15)) +
           labs(x=xlab,y="number of tweets",title="")
       }
-#      output$oid1 <- renderPrint({input$id1})
+    })
+    output$events <- renderPrint({
+      x <- input$inputX
+      y <- input$inputY
+      xlow  <- input$xlow
+      xhigh <- input$xhigh
+      ylow  <- input$ylow
+      yhigh <- input$yhigh
+
+      nEvents <- 0
+
+      if( y!='none' ){
+        rangeX = max(ukr[[x]]) - min(ukr[[x]])
+        rangeY = max(ukr[[y]]) - min(ukr[[y]])
+        ua <- subset(ukr, ukr[[x]] > min(ukr[[x]]) + xlow/100.*rangeX & ukr[[x]] < min(ukr[[x]]) + xhigh/100.*rangeX &
+                          ukr[[y]] > min(ukr[[y]]) + ylow/100.*rangeY & ukr[[y]] < min(ukr[[y]]) + yhigh/100.*rangeY)
+        nEvents <- dim(ua)[1]
+      } else {
+        rangeX = max(ukr[[x]]) - min(ukr[[x]])
+        ua <- subset(ukr, ukr[[x]] > min(ukr[[x]]) + xlow/100.*rangeX & ukr[[x]] < min(ukr[[x]]) + xhigh/100.*rangeX)
+        nEvents <- dim(ua)[1]
+      } 
+
+      paste(nEvents," events captured")
     })
   }
 )
