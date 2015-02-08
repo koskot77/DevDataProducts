7405537a9e9c66291dbaed46d9023cc47b2dea7e
koskot77 <koskot77@hotmail.com>
2015-02-08 10:55:42 +0100

    qwe

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 library(UsingR)
 library(ggplot2)
-ukr <- read.csv(file="ua.csv",header=T,sep=',')
+library(rCharts)
+
+ukr <- read.csv(file="ukr.txt", header=T, sep=',', encoding = "UTF-8", stringsAsFactors = F)
 ukr$dt  <- as.POSIXct( ukr$DateTime, format="%Y-%m-%d %H:%M:%S",  tz = "GMT")
 
 shinyServer(
@@ -33,8 +35,8 @@ shinyServer(
         ua <- subset(ukr, ukr[[x]] > min(ukr[[x]]) + xlow/100.*rangeX & ukr[[x]] < min(ukr[[x]]) + xhigh/100.*rangeX &
                           ukr[[y]] > min(ukr[[y]]) + ylow/100.*rangeY & ukr[[y]] < min(ukr[[y]]) + yhigh/100.*rangeY)
         ggplot(data=ua, aes_string(x=x,y=y)) + geom_point(alpha=0.1,color="blue") + 
-          theme(title = element_text(size = 15), axis.title.x = element_text(size = 15)) +
-          labs(x=xlab,y=ylab,title="")
+           theme(title = element_text(size = 15), axis.title.x = element_text(size = 15)) +
+           labs(x=xlab,y=ylab,title="")
       } else {
         rangeX = max(ukr[[x]]) - min(ukr[[x]])
         ua <- subset(ukr, ukr[[x]] > min(ukr[[x]]) + xlow/100.*rangeX & ukr[[x]] < min(ukr[[x]]) + xhigh/100.*rangeX)
@@ -44,6 +46,7 @@ shinyServer(
         nBins <- 1000
         if( x=='PostLength' )
             nBins = width
+
         ggplot(data=ua, aes_string(x=x)) + geom_histogram(binwidth = width/nBins,color="blue",fill="blue") + 
           theme(title = element_text(size = 15), axis.title.x = element_text(size = 15)) +
           labs(x=xlab,y="number of tweets",title="")
@@ -71,7 +74,54 @@ shinyServer(
         nEvents <- dim(ua)[1]
       } 
 
-      paste(nEvents," events captured")
+      if( nEvents > 100 )
+         paste(nEvents," events captured, sampling 100 random events below")
+      else
+         paste(nEvents," events captured, all are shown below")
     })
+
+    output$myChart = renderChart({
+
+      x <- input$inputX
+      y <- input$inputY
+      xlow  <- input$xlow
+      xhigh <- input$xhigh
+      ylow  <- input$ylow
+      yhigh <- input$yhigh
+
+      nEvents <- 0
+
+      if( y!='none' ){
+        rangeX = max(ukr[[x]]) - min(ukr[[x]])
+        rangeY = max(ukr[[y]]) - min(ukr[[y]])
+        ua <- subset(ukr, ukr[[x]] > min(ukr[[x]]) + xlow/100.*rangeX & ukr[[x]] < min(ukr[[x]]) + xhigh/100.*rangeX &
+                          ukr[[y]] > min(ukr[[y]]) + ylow/100.*rangeY & ukr[[y]] < min(ukr[[y]]) + yhigh/100.*rangeY)
+        nEvents <- dim(ua)[1]
+      } else {
+        rangeX = max(ukr[[x]]) - min(ukr[[x]])
+        ua <- subset(ukr, ukr[[x]] > min(ukr[[x]]) + xlow/100.*rangeX & ukr[[x]] < min(ukr[[x]]) + xhigh/100.*rangeX)
+        nEvents <- dim(ua)[1]
+      } 
+
+      if( nEvents>100 )
+        ua <- ua[runif(n = 100, min = 0, max = nEvents),] 
+
+      if( y=='none' ) y=x
+
+        if( x=="dt" ) x="DateTime"
+        if( y=="dt" ) y="DateTime"
+        d1 <- dPlot(x=x, y=y, groups = c("userId","Text"), data = ua, type = "bubble") #, height=800, width=1000)
+
+        if( x=='DateTime' )
+            d1$xAxis(type = "addTimeAxis", inputFormat = "%Y-%m-%d %H:%M:%S", outputFormat = "%Y-%m-%d %H:%M:%S")
+        if( y=='DateTime' )
+            d1$yAxis(type = "addTimeAxis", inputFormat = "%Y-%m-%d %H:%M:%S", outputFormat = "%Y-%m-%d %H:%M:%S")
+
+        d1$addParams(dom = 'myChart')
+
+        return(d1)
+
+    })
+
   }
 )
--- tweet_users.py
+++ tweet_users.py
@@ -14,7 +14,14 @@ def main():
   # load each tweet as json
   for line in open(sys.argv[1]):
     tweet_json = json.loads(line)
-    
+
+    emoji_dict = open('emoji.json','r')
+    emoji_json = json.loads(emoji_dict.readline())
+
+    emoji_list = {}
+    for emo in emoji_json.items():
+       emoji_list[ emo[1][0] ] = ":"+emo[1][1][0]+":"
+
     # only accept records with a 'user' field
     if tweet_json.get('user'):
       postId   = tweet_json['id']
@@ -24,6 +31,24 @@ def main():
       time_at  = re.sub(r'\+0000 ','', tweet_json['created_at'] )
       text     = tweet_json['text'].encode('utf8')
       text     = re.sub(r'"','``', text )
+      plainTxt = text.decode('utf8')
+
+      # decode emojis
+      for c in text.decode('utf8'):
+         if c in emoji_list :
+            plainTxt = re.sub(c, emoji_list[c], plainTxt)
+
+      # purge remaining unknown emojis
+      myre = re.compile(u'['
+         u'\U0001F300-\U0001F64F'
+         u'\U0001F680-\U0001F6FF'
+         u'\u2600-\u26FF\u2700-\u27BF'
+         u'\U000FE4EC]+', 
+         re.UNICODE)
+      #\ud83c\udf88\udbb9\udcec
+
+      text = myre.sub(':emoji:',plainTxt).encode('utf-8')
+
       place    = tweet_json['place']['name'].encode('utf8')
       placeId  = tweet_json['place']['id']
       post_time = time.strptime(time_at)
@@ -42,7 +67,7 @@ def main():
         continue
 
 ##      output.write( str(postId) + "," + str(userId) +  "," + str(len(text.decode("utf-8")))+ "," + str(time.strftime('%Y-%m-%d %H:%M:%S',post_time)) + "\n" )
-      output.write( str(postId) + "," + str(userId) + "," + str(geoCoord[0]) + "," + str(geoCoord[1])  + "," + str(len(text.decode("utf-8")))+ "," + str(time.strftime('%Y-%m-%d %H:%M:%S',post_time)) + ",\"" + text + "\"\n" )
+      output.write( str(postId) + "," + str(userId) + "," + str(geoCoord[0]) + "," + str(geoCoord[1])  + "," + str(len(text.decode("utf8")))+ "," + str(time.strftime('%Y-%m-%d %H:%M:%S',post_time)) + ",\"" + text + "\"\n" )
 #      output.write(time.strftime('%Y-%m-%d %H:%M:%S\n',post_time))
 
       if userName not in userActivity:
